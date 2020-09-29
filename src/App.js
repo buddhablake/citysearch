5f5c4a6bf9776b384d0b148899a1809b0c2f9154
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-09-29 10:03:10 -0700

    able to consume and display cities from CitiesContext in SearchResults Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { CitiesProvider } from "./context/CitiesContext";
+import SearchResults from "./components/SearchResults";
 
 const App = () => {
   return (
     <CitiesProvider>
-      <div className="App">Hello</div>
+      <div className="App">
+        <SearchResults />
+      </div>
     </CitiesProvider>
   );
 };
--- src/context/CitiesContext.js
+++ src/context/CitiesContext.js
@@ -1,9 +1,25 @@
-import React, { createContext, useState } from "react";
-import { getCities } from "../api";
+import React, { createContext, useState, useEffect } from "react";
+
+import axios from "axios";
+
 export const CitiesContext = createContext();
 
 export const CitiesProvider = (props) => {
-  const [cities, setCities] = useState(getCities());
+  const [cities, setCities] = useState();
+
+  useEffect(() => {
+    //Were we to be posting, updating, or deleting this data I would abstract this and other crud functions out into their own API file.
+    const getCities = async () => {
+      const res = await axios.get(
+        "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+      );
+
+      setCities(res.data);
+    };
+    getCities();
+
+    return () => {};
+  }, []);
 
   return (
     <CitiesContext.Provider value={{ cities, setCities }}>
